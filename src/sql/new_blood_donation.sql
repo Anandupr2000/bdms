d5928fb27c397c339d6a30e28f7e825b3f69fe7d
Anandu <ananduprajesh@gmail.com>
2023-07-27 13:42:36 +0530

    adding medical history table

--- src/sql/new_blood_donation.sql
+++ src/sql/new_blood_donation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2023 at 10:07 AM
+-- Generation Time: Jul 27, 2023 at 10:10 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -114,7 +114,7 @@ CREATE TABLE `donor_details` (
 
 INSERT INTO `donor_details` (`donor_id`, `uid`, `donor_number`, `donor_age`, `donor_gender`, `donor_blood`, `donor_address`, `fees`, `hascertificate`) VALUES
 (1, 2, '9091358908', 24, 'male', 4, 'Ernakulam', 0, 1),
-(2, 3, '9901582965', 23, 'female', 4, 'Alappuzha', 0, 0),
+(2, 3, '9901582965', 23, 'female', 4, 'Alappuzha', 100, 0),
 (3, 4, '9903998913', 24, 'male', 1, 'Wayanad', 200, 2),
 (4, 5, '941319558', 24, 'male', 3, 'Kannur', 100, 0),
 (5, 6, '9033315898', 24, 'male', 4, 'Pathanamthitta', 0, 0),
@@ -122,6 +122,38 @@ INSERT INTO `donor_details` (`donor_id`, `uid`, `donor_number`, `donor_age`, `do
 (7, 17, '8909127812', 27, 'Male', 8, 'Kasargod', 100, 0),
 (8, 23, '8967898976', 23, 'Male', 2, 'Thrissur', 0, 0),
 (13, 24, '0884656891', 25, 'Male', 5, 'Kochi', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medical_history`
+--
+
+CREATE TABLE `medical_history` (
+  `uid` varchar(10) NOT NULL,
+  `gudHealth` varchar(10) NOT NULL,
+  `bloodDonated` varchar(10) NOT NULL,
+  `sickness` varchar(10) NOT NULL,
+  `pregnancy` varchar(10) NOT NULL,
+  `diabetic` varchar(10) NOT NULL,
+  `std` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `medical_history`
+--
+
+INSERT INTO `medical_history` (`uid`, `gudHealth`, `bloodDonated`, `sickness`, `pregnancy`, `diabetic`, `std`) VALUES
+('2', 'Yes', 'No', 'No', 'No', 'No', 'No'),
+('3', 'Yes', 'No', 'No', 'No', 'No', 'No'),
+('4', 'Yes', 'No', 'No', 'No', 'No', 'No'),
+('5', 'Yes', 'No', 'No', 'No', 'No', 'No'),
+('6', 'Yes', 'No', 'No', 'No', 'No', 'No'),
+('7', 'Yes', 'No', 'No', 'No', 'No', 'No'),
+('17', 'Yes', 'No', 'No', 'No', 'No', 'No'),
+('18', 'Yes', 'No', 'No', 'No', 'No', 'No'),
+('23', 'Yes', 'No', 'No', 'No', 'No', 'No'),
+('24', 'Yes', 'No', 'No', 'No', 'No', 'No');
 
 -- --------------------------------------------------------
 
