2fc3e739ad0ce9ea12cac5346b69f2009547497e
Anandu <ananduprajesh@gmail.com>
2023-07-29 09:36:04 +0530

    re-establishing secure connection to db

--- src/conn.php
+++ src/conn.php
@@ -1,9 +1,9 @@
 <?php
 $servername = "mysql_bdms";
 $username = "root";
-$password = "root";
+$password = "root@Anandu#2000";
 $dbname = "blood_donation";
-$port = '3306';
+$port = 3306;
 // $conn=mysqli_connect("localhost","root","","blood_donation") or die("Connection error");
 $conn = new mysqli($servername, $username, $password, $dbname,$port);
 
